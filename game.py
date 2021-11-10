01a45bfd98c651576265f46a4a68c78ab3af0536
Brendan Smith <brendansmith@Brendans-MacBook-Air.local>
2021-11-10 17:09:49 -0600

    second step second challenge

--- game.py
+++ game.py
@@ -28,19 +28,44 @@ class Apple(GameObject):
         self.dy = (randint(0, 200) / 100) + 1
         self.reset()
 
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        if self.y > 500: 
+            self.reset()
 
+    def reset(self):
+        self.x = randint(50, 400)
+        self.y = -64
+
+class Strawberry(GameObject):
+  def __init__(self):
+    super(Strawberry, self).__init__(0, 0, 'strawberry.png')
+    self.dx = (randint(0, 200) / 100) + 1
+    self.dy = 0
+    self.reset()
+
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+    if self.x > 500:
+      self.reset()
+
+  def reset(self):
+    self.x = randint(50, 400)
+    self.y = -64  
 
 # Make an instance of GameObject
 # box = GameObject(120, 300, 50, 50) REMOVE!
-apple = GameObject(120, 300, 'apple.png') # ADD!
-strawberry = GameObject(160, 350, 'strawberry.png')
-# Clear screen
-screen.fill((255, 255, 255))
-# Draw box
-strawberry.render(screen)
-apple.render(screen) # ADD!
-# Update the window
-pygame.display.flip()
+apple1 = GameObject(0, 0, 'apple.png') # ADD!
+apple2 = GameObject(500, 30, 'apple.png') 
+apple3 = GameObject(480, 480, 'apple.png')
+apple4 = GameObject(0, 500, 'apple.png') 
+apple5 = GameObject(250, 250, 'apple.png') 
+strawberry1 = GameObject(15, 250, 'strawberry.png') # ADD!
+strawberry2 = GameObject(250, 15, 'strawberry.png')
+strawberry3 = GameObject(500, 250, 'strawberry.png')
+strawberry4 = GameObject(250, 500, 'strawberry.png')
 
 # Creat the game loop
 running = True
@@ -52,7 +77,15 @@ while running:
   # Clear screen
   screen.fill((255, 255, 255))
   # Draw apple
-  apple.render(screen)
+  strawberry1.render(screen)
+  strawberry2.render(screen)
+  strawberry3.render(screen)
+  strawberry4.render(screen)
+  apple1.render(screen)
+  apple2.render(screen)
+  apple3.render(screen)
+  apple4.render(screen)
+  apple5.render(screen)
   # Update the window
   pygame.display.flip()
 
